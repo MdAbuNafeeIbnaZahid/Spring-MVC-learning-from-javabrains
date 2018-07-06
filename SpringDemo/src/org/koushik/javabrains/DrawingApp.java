1c6034e9923d60f2a5815b089357c3b240332076
MdAbuNafeeIbnaZahid <1305023.maniz@ugrad.cse.buet.ac.bd>
2018-07-06 16:41:09 +0600

    Fixed bug of huge sized VMat

--- SpringDemo/src/org/koushik/javabrains/DrawingApp.java
+++ SpringDemo/src/org/koushik/javabrains/DrawingApp.java
@@ -2,28 +2,32 @@ package org.koushik.javabrains;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
 @SuppressWarnings("deprecation")
 public class DrawingApp {
 
 	public static void main(String[] args) {
-//		Triangle triangle = new Triangle();
 		
-		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
-		System.out.println("beanFactory created");
+//		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+//		System.out.println("beanFactory created");
 		
-		Triangle triangle1 = (Triangle) factory.getBean("triangle");
-		Triangle triangle2 = (Triangle) factory.getBean("triangle");
+		System.out.println("starting of main method");
 		
-		triangle1.draw();
-		
-		System.out.println(triangle1);
-		System.out.println(triangle2);
+		ApplicationContext context1 = new ClassPathXmlApplicationContext("spring.xml");
+		ApplicationContext context2 = new ClassPathXmlApplicationContext("spring.xml");
+		System.out.println("ApplicationContext created");
 		
 		
-//		triangle.draw();
-
+		System.out.println("autowire");
+		Triangle triangle1 = (Triangle) context1.getBean("triangle");
+		Triangle triangle2 = (Triangle) context2.getBean("triangle");
+		
+		System.out.println( triangle1 );
+		System.out.println( triangle2 );
+		
 	}
 
 }
--- SpringDemo/src/org/koushik/javabrains/Triangle.java
+++ SpringDemo/src/org/koushik/javabrains/Triangle.java
@@ -1,7 +1,72 @@
 package org.koushik.javabrains;
 
-public class Triangle {
-	public void draw() {
-		System.out.println("Triangle drawn");
+import java.util.List;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Triangle implements ApplicationContextAware, BeanNameAware {
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
+	
+	private ApplicationContext context = null;
+	private String beanName;
+	
+	
+	
+	public Triangle() {
+		System.out.println("default constructor of Triangle");
 	}
+
+	public Triangle(Point pointA, Point pointB, Point pointC) {
+		System.out.println("3 constructor of Triangle");
+		this.pointA = pointA;
+		this.pointB = pointB;
+		this.pointC = pointC;
+	}
+
+	public Point getPointA() {
+		return pointA;
+	}
+
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
+
+	@Override
+	public String toString() {
+		return "Triangle [pointA=" + pointA + ", pointB=" + pointB + ", pointC=" + pointC + ", beanName=" + beanName
+				+ "]";
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext context) throws BeansException {
+		this.context = context;
+	}
+
+	@Override
+	public void setBeanName(String beanName) {
+		this.beanName = beanName;
+	}
+
+
 }
